e7e946b28f4d90058a428f72c4608c3910d7a976
Faris Faikar <farisfaikar.r@gmail.com>
2024-09-03 16:49:46 +0700

    feat: add credits into navbar

--- components/nav/navbar.tsx
+++ components/nav/navbar.tsx
@@ -1,9 +1,19 @@
+import { badgeVariants } from "@/components/ui/badge";
+import Link from "next/link";
+import { MoveUpRight } from "lucide-react";
+
 export default function Navbar() {
 	return (
 		<nav className="fixed flex justify-center items-center w-full h-12 border-b bg-neutral-950/50 backdrop-blur-md border-neutral-700">
 			<div className="flex items-center gap-10 justify-between max-w-2xl w-full p-5">
-				<a href="/">WAYRY</a>
-				<div className="flex gap-10">
+				<div className="flex gap-4 items-center">
+					<a href="/">WAYRY</a>
+					<Link href="https://farisfaikar.vercel.app" target="_blank" className={`${badgeVariants({ variant: "secondary" })} flex gap-1`}>
+						<p className="sm:flex hidden">Made by Faris Faikar</p>
+						<MoveUpRight size={16} />
+					</Link>
+				</div>
+				<div className="flex gap-8">
 					<a href="/about" className="text-neutral-300">
 						About
 					</a>
