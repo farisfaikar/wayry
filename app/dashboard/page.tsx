f0d5ffefc5bec3ab0901542d7d927ce09be43afb
Faris Faikar <farisfaikar.r@gmail.com>
2024-09-04 20:40:07 +0700

    feat: add dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,24 @@
+import getSentences from "@/server/actions/get-sentences"
+import { CloudCog } from "lucide-react";
+
+export default async function DashboardPage() {
+	const { error, success } = await getSentences()
+	
+	if (error) {
+		throw new Error(error)
+	}
+
+	console.log(success)
+	if (success) {
+		return (
+			<div className="mt-5">
+				{success.map((sentence) => (
+					<div key={sentence.id}>
+						<h2>{sentence.sentence}</h2>
+						<p>{sentence.person}</p>
+					</div>
+				))}
+			</div>				
+		)
+	}
+}
