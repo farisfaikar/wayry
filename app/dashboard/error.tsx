a9b01b1b1b32725f94c371d549d4a030ea49afb0
Faris Faikar <farisfaikar.r@gmail.com>
2024-09-04 20:39:44 +0700

    feat: add error page for dashboard

---
+++ app/dashboard/error.tsx
@@ -0,0 +1,29 @@
+"use client"
+import { Button } from "@/components/ui/button"
+import { useEffect } from "react"
+import { RotateCcw } from "lucide-react"
+
+export default function Error({
+   error,
+   reset
+}: {
+   error: Error & { digest?: string },
+   reset: () => void
+}) {
+
+   useEffect(() => {
+     console.error(error)
+   },[error])
+
+  return (
+    <div className="absolute inset-0 -z-10 mx-auto flex max-w-xl items-center justify-center">
+      <div className="flex h-20 items-center border-r border-secondary p-5 text-2xl">400</div>
+      <p className="p-5 text-sm">
+        Something went wrong!
+      </p>
+      <Button variant="outline" onClick={reset} className="flex gap-2 items-center">
+        <RotateCcw size={16} /> Try again
+      </Button>
+    </div>
+  );
+}
