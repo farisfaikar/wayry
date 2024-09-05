f9feb04b927b76b3d675602210f7723e80d71245
Faris Faikar <farisfaikar.r@gmail.com>
2024-09-05 17:00:48 +0700

    db: add sentenceCount, elapsedTime, and sentencesPerMinute to sentences table

---
+++ server/migrations/0002_slow_stellaris.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "sentences" ADD COLUMN "sentence_count" text;--> statement-breakpoint
+ALTER TABLE "sentences" ADD COLUMN "elapsed_time" text;--> statement-breakpoint
+ALTER TABLE "sentences" ADD COLUMN "sentences_per_minute" text;
--- server/schema.ts
+++ server/schema.ts
@@ -4,4 +4,7 @@ export const sentences = pgTable('sentences', {
   id: serial('id').primaryKey().notNull(),
   sentence: text('sentence').notNull(),
   person: text('person'),
+  sentenceCount: text('sentence_count'),
+  elapsedTime: text('elapsed_time'),
+  sentencesPerMinute: text('sentences_per_minute'),
 })
