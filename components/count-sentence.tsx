04720d511aa318c276167f62e2b59100106255c7
Faris Faikar <farisfaikar.r@gmail.com>
2024-09-05 17:48:38 +0700

    feat: overhaul count-sentencen to include new framer-motion button

--- components/count-sentence.tsx
+++ components/count-sentence.tsx
@@ -1,6 +1,15 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
 import { Button } from '@/components/ui/button'
 import { useState, useEffect, useRef } from 'react'
-import { Play, Pause, RotateCcw, Save } from 'lucide-react'
+import { Play, Pause, RotateCcw, Save, Loader } from 'lucide-react'
+import createSentence from '@/server/actions/create-sentence'
+import { useRouter } from 'next/navigation'
+import CountButton from './count-button'
 
 type CountSentenceProps = {
   className?: string
@@ -10,12 +19,22 @@ type CountSentenceProps = {
   }
 }
 
+const formSchema = z.object({
+  sentence: z.string().min(1, 'Sentence is required.'),
+  person: z.string().nullable(),
+  sentenceCount: z.number().nullable(),
+  elapsedTime: z.number().nullable(),
+  sentencesPerMinute: z.number().nullable(),
+})
+
 export default function CountSentence({ className = '', data }: CountSentenceProps) {
   const [sentenceCount, setSentenceCount] = useState(0)
   const [elapsedTime, setElapsedTime] = useState(0)
   const [sentencesPerMinute, setSentencesPerMinute] = useState(0)
   const [isPlaying, setIsPlaying] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
   const intervalRef = useRef<NodeJS.Timeout | null>(null)
+  const router = useRouter()
 
   // Start the timer when the component mounts
   useEffect(() => {
@@ -57,31 +76,64 @@ export default function CountSentence({ className = '', data }: CountSentencePro
     setIsPlaying(false)
   }
 
+  // Define form
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      sentence: '',
+      person: null,
+    },
+  })
+
+  // Define submit handler
+  const handleCreateSentence = async () => {
+    setIsLoading(true)
+    setIsPlaying(false)
+
+    const newData = {
+      person: data.person,
+      sentence: data.sentence,
+      sentenceCount,
+      elapsedTime,
+      sentencesPerMinute,
+    }
+
+    const response = await createSentence(newData)
+
+    setIsLoading(false)
+
+    if (response.success) {
+      router.push('/dashboard')
+    } else {
+      console.error('Failed to create sentence: ', response.error)
+    }
+  }
+
   return (
     <div className={className}>
-      <Button
-        className="w-full text-wrap p-10 text-center text-3xl font-bold"
-        onClick={handleButtonClick} // Use the click handler function
+      <CountButton
+        className="w-full text-wrap p-10 pt-7 text-center text-3xl font-bold h-full flex flex-col gap-2"
+        onClick={handleButtonClick}
         disabled={!isPlaying}
       >
         {data.sentence}
-      </Button>
+      </CountButton>
       <div className="mt-5 flex flex-col gap-2">
         <div className="flex items-center justify-between gap-2">
-          <p className="text-neutral-500">Person</p>
-          <p className="text-xl font-bold">{data.person !== '' ? data.person : <i>Unknown</i>}</p>
+          <p className="text-sm text-neutral-500">Person</p>
+          <p className="text-lg font-bold">{data.person !== '' ? data.person : <i>Unknown</i>}</p>
         </div>
         <div className="flex items-center justify-between gap-2">
-          <p className="text-neutral-500">Sentence count</p>
-          <p className="text-xl font-bold">{sentenceCount}</p>
+          <p className="text-sm text-neutral-500">Sentence count</p>
+          <p className="text-lg font-bold">{sentenceCount}</p>
         </div>
         <div className="flex items-center justify-between gap-2">
-          <p className="text-neutral-500">Elapsed Time</p>
-          <p className="text-xl font-bold">{elapsedTime} seconds</p>
+          <p className="text-sm text-neutral-500">Elapsed time</p>
+          <p className="text-lg font-bold">{elapsedTime} seconds</p>
         </div>
         <div className="flex items-center justify-between gap-2">
-          <p className="text-neutral-500">Sentences Per Minute</p>
-          <p className="text-xl font-bold">{sentencesPerMinute.toFixed(2)}</p>
+          <p className="text-sm text-neutral-500">Sentences per minute</p>
+          <p className="text-lg font-bold">{sentencesPerMinute.toFixed(2)}</p>
         </div>
       </div>
       <div className="mt-5 flex flex-col justify-between gap-5 sm:flex-row">
@@ -89,6 +141,7 @@ export default function CountSentence({ className = '', data }: CountSentencePro
           variant={isPlaying ? 'outline' : 'default'}
           className="flex w-full gap-2"
           onClick={togglePlaying}
+          disabled={isLoading}
         >
           {isPlaying ? (
             <>
@@ -100,11 +153,29 @@ export default function CountSentence({ className = '', data }: CountSentencePro
             </>
           )}
         </Button>
-        <Button variant="outline" className="flex w-full gap-2" onClick={resetTimer}>
+        <Button
+          variant="outline"
+          onClick={resetTimer}
+          className="flex w-full gap-2"
+          disabled={isLoading}
+        >
           <RotateCcw /> Reset
         </Button>
-        <Button variant="outline" className="flex w-full gap-2" disabled>
-          <Save /> Save
+        <Button
+          variant="outline"
+          onClick={handleCreateSentence}
+          className="flex w-full gap-2"
+          disabled={isLoading} // Disable button while loading
+        >
+          {isLoading ? (
+            <>
+              <Loader className="animate-spin" /> Saving...
+            </>
+          ) : (
+            <>
+              <Save /> Save
+            </>
+          )}
         </Button>
       </div>
     </div>
