4da1168b963b782d3eb156b359ee526f42f82ea1
Faris Faikar <farisfaikar.r@gmail.com>
2024-09-06 21:20:07 +0700

    db: add schema for user data

--- server/schema.ts
+++ server/schema.ts
@@ -1,10 +1,61 @@
-import { pgTable, serial, text } from 'drizzle-orm/pg-core'
+import {
+  boolean,
+  timestamp,
+  pgTable,
+  text,
+  primaryKey,
+  integer,
+  serial,
+  doublePrecision,
+} from "drizzle-orm/pg-core"
+import postgres from "postgres"
+import { drizzle } from "drizzle-orm/postgres-js"
+import type { AdapterAccount } from "next-auth/adapters"
+
+const connectionString = "postgres://postgres:postgres@localhost:5432/drizzle"
+const pool = postgres(connectionString, { max: 1 })
+ 
+export const db = drizzle(pool)
+
+export const users = pgTable("user", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  name: text("name"),
+  email: text("email").unique(),
+  emailVerified: timestamp("emailVerified", { mode: "date" }),
+  image: text("image"),
+})
+ 
+export const accounts = pgTable(
+  "account",
+  {
+    userId: text("userId")
+      .notNull()
+      .references(() => users.id, { onDelete: "cascade" }),
+    type: text("type").$type<AdapterAccount>().notNull(),
+    provider: text("provider").notNull(),
+    providerAccountId: text("providerAccountId").notNull(),
+    refresh_token: text("refresh_token"),
+    access_token: text("access_token"),
+    expires_at: integer("expires_at"),
+    token_type: text("token_type"),
+    scope: text("scope"),
+    id_token: text("id_token"),
+    session_state: text("session_state"),
+  },
+  (account) => ({
+    compoundKey: primaryKey({
+      columns: [account.provider, account.providerAccountId],
+    }),
+  })
+)
 
 export const sentences = pgTable('sentences', {
   id: serial('id').primaryKey().notNull(),
   sentence: text('sentence').notNull(),
   person: text('person'),
-  sentenceCount: text('sentence_count'),
-  elapsedTime: text('elapsed_time'),
-  sentencesPerMinute: text('sentences_per_minute'),
+  sentenceCount: integer('sentence_count'),
+  elapsedTime: integer('elapsed_time'),
+  sentencesPerMinute: doublePrecision('sentences_per_minute'),
 })
