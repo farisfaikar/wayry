1bfffdec9ed3c108ace08de68bd5b36068607e5c
Faris Faikar <farisfaikar.r@gmail.com>
2024-09-07 22:59:52 +0700

    feat: set allowDangerousEmailAccountLinking to true in for google and github auth

--- server/auth.ts
+++ server/auth.ts
@@ -12,10 +12,12 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
     }),
     Github({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+      allowDangerousEmailAccountLinking: true,
     })
   ],
 })
