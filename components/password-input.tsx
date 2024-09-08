d309444eb9f95c82ebef1ccac025feb2cc98b8f9
Faris Faikar <farisfaikar.r@gmail.com>
2024-09-08 15:06:33 +0700

    fix: eye button on password-input not working correctly

--- components/password-input.tsx
+++ components/password-input.tsx
@@ -10,7 +10,7 @@ import { cn } from '@/lib/utils'
 const PasswordInput = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, ...props }, ref) => {
     const [showPassword, setShowPassword] = React.useState(false)
-    const disabled = props.value === '' || props.value === undefined || props.disabled
+    const isDisabled = props.disabled
 
     return (
       <div className="relative">
@@ -26,14 +26,16 @@ const PasswordInput = React.forwardRef<HTMLInputElement, InputProps>(
           size="sm"
           className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
           onClick={() => setShowPassword((prev) => !prev)}
-          disabled={disabled}
+          disabled={isDisabled}
         >
-          {showPassword && !disabled ? (
+          {showPassword ? (
             <EyeIcon className="h-4 w-4" aria-hidden="true" />
           ) : (
             <EyeOffIcon className="h-4 w-4" aria-hidden="true" />
           )}
-          <span className="sr-only">{showPassword ? 'Hide password' : 'Show password'}</span>
+          <span className="sr-only">
+            {showPassword ? 'Hide password' : 'Show password'}
+          </span>
         </Button>
 
         {/* hides browsers password toggles */}
