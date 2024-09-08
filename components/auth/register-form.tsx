23b6f11aa5b9aef6fbfae48753b01bd30324d136
Faris Faikar <farisfaikar.r@gmail.com>
2024-09-09 00:27:43 +0700

    feat: add email register

---
+++ components/auth/register-form.tsx
@@ -0,0 +1,128 @@
+'use client'
+
+import AuthCard from '@/components/auth/auth-card'
+import { Input } from '@/components/ui/input'
+import { PasswordInput } from '@/components/password-input'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { RegisterSchema } from '@/types/register-schema'
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+} from '@/components/ui/form'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import { z } from 'zod'
+import { useAction } from 'next-safe-action/hooks'
+import { cn } from '@/lib/utils'
+import { useState } from "react"
+import { emailRegister } from "@/server/actions/email-register"
+import SuccessForm from "@/components/auth/success-form"
+import ErrorForm from "@/components/auth/error-form"
+
+export default function RegisterForm() {
+  const [ error, setError ] = useState('')
+  const [ success, setSuccess ] = useState('')
+
+  const { execute, status } = useAction(emailRegister, {
+    onSuccess(data) {
+      if (data.error) setError(data.error)
+      if (data.success) setSuccess(data.success)
+    }
+  })
+  
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  })
+
+  const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
+    execute(values)
+  }
+
+  return (
+    <AuthCard
+      cardTitle="Create a WAYRY account!"
+      cardDescription="Enter your name, email and password to register."
+      backButtonHref="/auth/login"
+      backButtonLabel="Log into existing account"
+      showSocials
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    placeholder="John Doe"
+                    type="name"
+                  /> 
+                </FormControl>
+                <FormDescription />
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    placeholder="johndoe@gmail.com"
+                    type="email"
+                    autoComplete="email"
+                  />
+                </FormControl>
+                <FormDescription />
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <PasswordInput
+                    {...field}
+                    placeholder="********"
+                    autoComplete="current-password"
+                  />
+                </FormControl>
+                <FormDescription />
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <SuccessForm message={success} />
+          <ErrorForm message={error} />
+          <div className="mt-5">
+            <Button className={cn('w-full', status === 'executing' ? 'animate-pulse' : '')}>
+              Register
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </AuthCard>
+  )
+}
---
+++ lib/base-url.ts
@@ -0,0 +1,5 @@
+export default function getBaseURL() {
+  if (typeof window !== 'undefined') return ''
+  if (process.env.VERCEL_URL) return `https://${process.env.DOMAIN_URL}`
+  ;('http://localhost:3000')
+}
