4ae0e3caa32b2e616c1d9840eb474db14e2cf798
Faris Faikar <farisfaikar.r@gmail.com>
2024-09-09 19:08:47 +0700

    fix: add w-full to error and success form

--- components/auth/error-form.tsx
+++ components/auth/error-form.tsx
@@ -4,7 +4,7 @@ export default function ErrorForm({ message }: { message?: string }) {
   if (!message) return null
 
   return (
-    <div className="mt-5 flex items-center gap-2 rounded-md bg-destructive p-3 text-sm text-secondary-foreground">
+    <div className="mt-5 flex w-full items-center gap-2 rounded-md bg-destructive p-3 text-sm text-secondary-foreground">
       <AlertCircle className="h-4 w-4" />
       <p>{message}</p>
     </div>
--- components/auth/success-form.tsx
+++ components/auth/success-form.tsx
@@ -4,7 +4,7 @@ export default function SuccessForm({ message }: { message?: string }) {
   if (!message) return null
 
   return (
-    <div className="mt-5 flex items-center gap-2 rounded-md bg-secondary p-3 text-sm text-secondary-foreground">
+    <div className="mt-5 flex w-full items-center gap-2 rounded-md bg-secondary p-3 text-sm text-secondary-foreground">
       <CheckCircle className="h-4 w-4" />
       <p>{message}</p>
     </div>
