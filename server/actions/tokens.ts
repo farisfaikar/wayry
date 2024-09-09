b08ec94be6cd499cfa227b49fc270e8c5ccbc6cc
Faris Faikar <farisfaikar.r@gmail.com>
2024-09-09 19:09:55 +0700

    feat: add emailVerification function to tokens.ts

--- server/actions/tokens.ts
+++ server/actions/tokens.ts
@@ -1,13 +1,14 @@
 'use server'
 
+import { users } from '@/server/schema';
 import { db } from "@/server"
 import { emailTokens } from "@/server/schema"
 import { eq } from "drizzle-orm"
 
-export const getVerificationTokenByEmail = async (email: string) => {
+export const getVerificationTokenByEmail = async (token: string) => {
   try {
     const verificationToken = await db.query.emailTokens.findFirst({
-      where: eq(emailTokens.token, email)
+      where: eq(emailTokens.token, token)
     })
     return verificationToken
   } catch (error) {
@@ -35,4 +36,34 @@ export const generateEmailVerificationToken = async (email: string) => {
     .returning()
 
   return verificationToken
+}
+
+export const emailVerification = async (token: string) => {
+  const existingToken = await getVerificationTokenByEmail(token)
+
+  if (!existingToken) return { error: 'Token not found' }
+
+  const hasExpired = new Date(existingToken.expires) < new Date()
+
+  if (hasExpired) return { error: 'Token has expired' }
+
+  try {
+    const existingUser = await db.query.users.findFirst({
+      where: eq(users.email, existingToken.email)
+    })
+
+    if (!existingUser) return { error: 'Email does not exist' }
+  } catch (error) {
+    console.log(error)
+    return { error: 'Error fetching existing user' }
+  }
+
+  await db.update(users).set({
+    emailVerified: new Date,
+    email: existingToken.email,
+  })
+
+  await db.delete(emailTokens).where(eq(emailTokens.id, existingToken.id))
+
+  return { success: 'Email verified' }
 }
