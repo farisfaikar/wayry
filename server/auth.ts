a4f5d581ca8778baaf57e406c2ed08705ba5acd1
Faris Faikar <farisfaikar.r@gmail.com>
2024-09-09 20:41:43 +0700

    feat: add credentials to auth.ts

--- server/auth.ts
+++ server/auth.ts
@@ -1,13 +1,18 @@
-import NextAuth from "next-auth"
-import { DrizzleAdapter } from "@auth/drizzle-adapter"
-import { db } from "@/server"
-import Google from "next-auth/providers/google"
-import Github from "next-auth/providers/github"
+import { users } from '@/server/schema';
+import NextAuth from 'next-auth'
+import { DrizzleAdapter } from '@auth/drizzle-adapter'
+import { db } from '@/server'
+import Google from 'next-auth/providers/google'
+import Github from 'next-auth/providers/github'
+import Credentials from 'next-auth/providers/credentials'
+import { LoginSchema } from "@/types/login-schema"
+import { eq } from 'drizzle-orm'
+import bcrypt from 'bcrypt'
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: DrizzleAdapter(db),
   secret: process.env.AUTH_SECRET,
-  session: { strategy: "jwt" },
+  session: { strategy: 'jwt' },
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -18,6 +23,27 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
       allowDangerousEmailAccountLinking: true,
-    })
+    }),
+    Credentials({
+      authorize: async (credentials) => {
+        const validatedFields = LoginSchema.safeParse(credentials)
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data
+
+          const user = await db.query.users.findFirst({
+            where: eq(users.email, email)
+          })
+          
+          if (!user || !user.password) return null
+          
+          const passwordMatch = await bcrypt.compare(password, user.password)
+
+          if (passwordMatch) return user
+        }
+
+        return null
+      }
+    }),
   ],
 })
