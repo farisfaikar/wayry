196e482bbba6fb3051a6aa5d3a0aa9858edae18f
Faris Faikar <farisfaikar.r@gmail.com>
2024-09-09 19:09:42 +0700

    refactor: change new-verification to email-verification

--- server/actions/email-verification.ts
+++ server/actions/email-verification.ts
@@ -7,7 +7,7 @@ const resend = new Resend(process.env.RESEND_API_KEY)
 const domain = getBaseURL()
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const confirmLink = `${domain}/auth/new-verification?token=${token}`
+  const confirmLink = `${domain}/auth/email-verification?token=${token}`
   const { data, error } = await resend.emails.send({
     from: 'onboarding@resend.dev',
     to: email,
