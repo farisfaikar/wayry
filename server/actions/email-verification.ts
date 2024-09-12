0d5bb4afb8436010b78d2e7891e3d8af0cf3060b
Faris Faikar <farisfaikar.r@gmail.com>
2024-09-13 00:22:59 +0700

    feat: use react-email components for email-verification action

--- server/actions/email-verification.ts
+++ server/actions/email-verification.ts
@@ -1,5 +1,7 @@
 'use server'
 
+import ResetPasswordEmail from "@/components/emails/reset-password-email"
+import VerifyEmail from "@/components/emails/verify-email"
 import getBaseURL from '@/lib/base-url'
 import { Resend } from 'resend'
 
@@ -7,13 +9,13 @@ const resend = new Resend(process.env.RESEND_API_KEY)
 const domain = getBaseURL()
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const confirmLink = `${domain}/auth/email-verification?token=${token}`
+  const verifyEmailLink = `${domain}/auth/email-verification?token=${token}`
   const { data, error } = await resend.emails.send({
     from: 'onboarding@resend.dev',
     to: email,
-    subject: 'WAYRY | Confirm your email',
-    // react: EmailTemplate({ firstName: 'John' }),
-    html: `<p>Click here to <a href="${confirmLink}">confirm your email</a></p>`,
+    subject: 'WAYRY | Verify your email',
+    react: VerifyEmail({ verifyEmailLink, email }),
+    html: `<p>Click here to <a href="${verifyEmailLink}">verify your email</a></p>`,
   })
 
   if (error) return console.log(error)
@@ -21,13 +23,13 @@ export const sendVerificationEmail = async (email: string, token: string) => {
 }
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
-  const confirmLink = `${domain}/auth/reset-password?token=${token}`
+  const resetPasswordLink = `${domain}/auth/reset-password?token=${token}`
   const { data, error } = await resend.emails.send({
     from: 'onboarding@resend.dev',
     to: email,
-    subject: 'WAYRY | Confirm your email',
-    // react: EmailTemplate({ firstName: 'John' }),
-    html: `<p>Click here to <a href="${confirmLink}">reset your password</a></p>`,
+    subject: 'WAYRY | Reset your password',
+    react: ResetPasswordEmail({ resetPasswordLink, email }),
+    html: `<p>Click here to <a href="${resetPasswordLink}">reset your password</a></p>`,
   })
 
   if (error) return console.log(error)
