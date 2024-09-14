1fe867a52401d534d4e5fe27ee1b7df7a6676caf
Faris Faikar <farisfaikar.r@gmail.com>
2024-09-14 16:00:36 +0700

    feat: add user_id to the equation for create and get sentence server actions

--- server/actions/create-sentence.ts
+++ server/actions/create-sentence.ts
@@ -1,14 +1,17 @@
 'use server'
 
 import { db } from '@/server'
-import { sentences } from '../schema'
+import { sentences, users } from '@/server/schema'
+import { auth } from '@/server/auth'
+import { eq } from 'drizzle-orm'
+import { revalidatePath } from 'next/cache'
 
 type Data = {
-  person: string,
-  sentence: string,
-  sentenceCount: number,
-  elapsedTime: number,
-  sentencesPerMinute: number,
+  person: string
+  sentence: string
+  sentenceCount: number
+  elapsedTime: number
+  sentencesPerMinute: number
 }
 
 export default async function createSentence(data: Data) {
@@ -17,20 +20,32 @@ export default async function createSentence(data: Data) {
   const sentenceCount = data.sentenceCount
   const elapsedTime = data.elapsedTime
   const sentencesPerMinute = Number(data.sentencesPerMinute.toFixed(2))
-  
+  const session = await auth()
+  const userEmail = session?.user?.email
+  const user = await db.query.users.findFirst({
+    where: eq(users.email, userEmail ?? ''),
+  })
+
+  if (!session) return { error: 'Please login to create sentence' }
+
   if (!sentence) {
     return { error: 'No sentences found.' }
   }
-  
+
+  if (!user) return { error: 'User not found' }
+
   const insertedSentence = await db.insert(sentences).values({
-    sentence, 
+    userId: user.id,
+    sentence,
     person,
-    sentenceCount, 
-    elapsedTime, 
+    sentenceCount,
+    elapsedTime,
     sentencesPerMinute,
   })
 
-  const plainInsertedSentence = JSON.parse(JSON.stringify(insertedSentence));
+  const plainInsertedSentence = JSON.parse(JSON.stringify(insertedSentence))
 
-  return { success: plainInsertedSentence };
+  revalidatePath('/dashboard')
+
+  return { success: plainInsertedSentence }
 }
--- server/actions/get-sentences.ts
+++ server/actions/get-sentences.ts
@@ -1,13 +1,25 @@
 'use server'
 
 import { db } from '@/server'
+import { auth } from "@/server/auth"
+import { sentences, users } from "@/server/schema";
+import { eq } from 'drizzle-orm';
 
 export default async function getSentences() {
-  const sentences = await db.query.sentences.findMany()
+  const session = await auth()
+  const user = await db.query.users.findFirst({
+    where: eq(users.email, session?.user?.email ?? ''),
+  })
 
-  if (!sentences) {
+  if (!user) return { error: 'User not found' }
+  
+  const userSentences = await db.query.sentences.findMany({
+    where: eq(sentences.userId, user.id)
+  })
+
+  if (!userSentences) {
     return { error: 'No sentences found' }
   }
 
-  return { success: sentences }
+  return { success: userSentences }
 }
