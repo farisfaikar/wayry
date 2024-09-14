4b503e1f7042d940f3d13fc5762e1a13bfb5a299
Faris Faikar <farisfaikar.r@gmail.com>
2024-09-15 02:14:55 +0700

    remove: useless comments

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import getSentences from '@/server/actions/get-sentences'
+import getPeopleWithSentences from '@/server/actions/get-people-with-sentences'
 import createSentence from '@/server/actions/create-sentence'
 import {
   Accordion,
@@ -19,7 +20,7 @@ export const metadata: Metadata = {
 }
 
 export default async function DashboardPage() {
-  const { error, success } = await getSentences()
+  const { error, success } = await getPeopleWithSentences()
 
   if (error) {
     throw new Error(error)
@@ -28,43 +29,48 @@ export default async function DashboardPage() {
   // Check if success is empty
   if (success && success.length === 0) {
     return (
-      <div className="absolute inset-0 flex flex-col items-center justify-center h-full">
+      <div className="absolute inset-0 flex h-full flex-col items-center justify-center">
         <h2 className="text-xl font-bold">No Sentences Found :(</h2>
         <p className="text-neutral-500">There are currently no sentences available.</p>
         <Button variant="outline" className="mt-2">
-          <Link href="/" className="flex gap-2 items-center"><CirclePlus size={20} />Create sentence</Link>
+          <Link href="/" className="flex items-center gap-2">
+            <CirclePlus size={20} />
+            Create sentence
+          </Link>
         </Button>
       </div>
     )
   }
-  
+
   if (success) {
     return (
       <Accordion type="single" collapsible className="w-full">
-        {success.map((sentence) => (
-          <AccordionItem key={sentence.id} value={`sentence-${sentence.id}`}>
-            <AccordionTrigger>
-              {sentence.person !== '' ? sentence.person : <i>Unknown</i>}
-            </AccordionTrigger>
+        {success.map((person) => (
+          <AccordionItem key={person.id} value={`person-${person.id}`}>
+            <AccordionTrigger>{person.name !== '' ? person.name : <i>Unknown</i>}</AccordionTrigger>
             <AccordionContent>
-              <div>
-                <div className="flex items-center justify-between">
-                <span className="text-neutral-500">Sentence</span>
-                  <span className="text-xl font-bold">{sentence.sentence}</span>
+              {person.sentences.map((sentence) => (
+                <div>
+                  <div className="flex items-center justify-between">
+                    <span className="text-neutral-500">Sentence</span>
+                    <span className="text-xl font-bold">{sentence.sentence}</span>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <span className="text-neutral-500">Sentence count</span>
+                    <span className="text-xl font-bold">{sentence.sentenceCount}</span>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <span className="text-neutral-500">Elapsed time</span>
+                    <span className="text-xl font-bold">{sentence.elapsedTime}</span>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <span className="text-neutral-500">Sentences per minute</span>
+                    <span className="text-xl font-bold">
+                      {sentence.sentencesPerMinute?.toFixed(2)}
+                    </span>
+                  </div>
                 </div>
-                <div className="flex items-center justify-between">
-                  <span className="text-neutral-500">Sentence count</span>
-                  <span className="text-xl font-bold">{sentence.sentenceCount}</span>
-                </div>
-                <div className="flex items-center justify-between">
-                  <span className="text-neutral-500">Elapsed time</span>
-                  <span className="text-xl font-bold">{sentence.elapsedTime}</span>
-                </div>
-                <div className="flex items-center justify-between">
-                  <span className="text-neutral-500">Sentences per minute</span>
-                  <span className="text-xl font-bold">{sentence.sentencesPerMinute?.toFixed(2)}</span>
-                </div>
-              </div>
+              ))}
             </AccordionContent>
           </AccordionItem>
         ))}
