5b38655d44b4c92008a59f26f03b608ab2927515
Faris Faikar <farisfaikar.r@gmail.com>
2024-09-14 15:33:02 +0700

    db: revert migrations

--- server/migrations/0000_previous_proteus.sql
+++ server/migrations/0000_previous_proteus.sql
@@ -4,11 +4,11 @@ EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "accounts" (
-	"user_id" text NOT NULL,
+CREATE TABLE IF NOT EXISTS "account" (
+	"userId" text NOT NULL,
 	"type" text NOT NULL,
 	"provider" text NOT NULL,
-	"provider_account_id" text NOT NULL,
+	"providerAccountId" text NOT NULL,
 	"refresh_token" text,
 	"access_token" text,
 	"expires_at" integer,
@@ -16,14 +16,23 @@ CREATE TABLE IF NOT EXISTS "accounts" (
 	"scope" text,
 	"id_token" text,
 	"session_state" text,
-	CONSTRAINT "accounts_provider_provider_account_id_pk" PRIMARY KEY("provider","provider_account_id")
+	CONSTRAINT "account_provider_providerAccountId_pk" PRIMARY KEY("provider","providerAccountId")
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "email_tokens" (
 	"id" text NOT NULL,
 	"token" text NOT NULL,
 	"expires" timestamp NOT NULL,
+	"email" text NOT NULL,
 	CONSTRAINT "email_tokens_id_token_pk" PRIMARY KEY("id","token")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "password_reset_tokens" (
+	"id" text NOT NULL,
+	"token" text NOT NULL,
+	"expires" timestamp NOT NULL,
+	"email" text NOT NULL,
+	CONSTRAINT "password_reset_tokens_id_token_pk" PRIMARY KEY("id","token")
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "sentences" (
@@ -35,19 +44,28 @@ CREATE TABLE IF NOT EXISTS "sentences" (
 	"sentences_per_minute" double precision
 );
 --> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "users" (
+CREATE TABLE IF NOT EXISTS "two_factor_tokens" (
+	"id" text NOT NULL,
+	"token" text NOT NULL,
+	"expires" timestamp NOT NULL,
+	"email" text NOT NULL,
+	CONSTRAINT "two_factor_tokens_id_token_pk" PRIMARY KEY("id","token")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "user" (
 	"id" text PRIMARY KEY NOT NULL,
 	"name" text,
 	"email" text,
-	"email_verified" timestamp,
+	"password" text,
+	"emailVerified" timestamp,
 	"image" text,
 	"two_factor_enabled" boolean DEFAULT false,
 	"roles" "roles" DEFAULT 'user',
-	CONSTRAINT "users_email_unique" UNIQUE("email")
+	CONSTRAINT "user_email_unique" UNIQUE("email")
 );
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "account" ADD CONSTRAINT "account_userId_user_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
--- server/migrations/0001_familiar_puff_adder.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "email_tokens" ADD COLUMN "email" text NOT NULL;
