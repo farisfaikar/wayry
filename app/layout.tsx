a1508ce98f26e94418c4ce619723be3d4078bf84
Faris Faikar <farisfaikar.r@gmail.com>
2024-09-14 15:59:17 +0700

    feat: add SessionProvider to layout to encapsulate everything inside SessionProvider so that session actually fucking works

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
 import Navbar from '@/components/nav/navbar'
+import { SessionProvider } from 'next-auth/react'
 
 export const metadata: Metadata = {
   title: 'Why Are You Repeating Yourself?',
@@ -18,7 +19,11 @@ export default function RootLayout({
       <body>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           <Navbar />
-          <main className="mx-auto max-w-2xl p-5 pt-16">{children}</main>
+          <main className="mx-auto max-w-2xl p-5 pt-16">
+            <SessionProvider>
+              {children}
+            </SessionProvider>
+          </main>
         </ThemeProvider>
       </body>
     </html>
