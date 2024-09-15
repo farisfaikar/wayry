5ba0e1a5c893577917e2b7c1f13fc27940ac66d3
Faris Faikar <farisfaikar.r@gmail.com>
2024-09-15 20:23:42 +0700

    feat: make sentence-table responsive for mobile screens

--- components/sentence-table.tsx
+++ components/sentence-table.tsx
@@ -13,11 +13,11 @@ import {
 import { Button } from '@/components/ui/button'
 import { ChevronDown, ChevronUp } from 'lucide-react'
 import { useState } from 'react'
-import { useToast } from '@/hooks/use-toast'
+import { useMediaQuery } from 'react-responsive'
 import EditPersonDialog from '@/components/dialogs/edit-person-dialog'
 import EditSentenceDialog from '@/components/dialogs/edit-sentence-dialog'
 import DeleteSentenceDialog from '@/components/dialogs/delete-sentence-dialog'
-import DeletePersonDialog from "@/components/dialogs/delete-person-dialog"
+import DeletePersonDialog from '@/components/dialogs/delete-person-dialog'
 
 type Sentence = {
   id: number
@@ -41,9 +41,7 @@ type SentenceTableProps = {
 
 export default function SentenceTable({ people }: SentenceTableProps) {
   const [expandedRows, setExpandedRows] = useState<number[]>([])
-  const [personToDelete, setPersonToDelete] = useState<Person | null>(null)
-  const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false)
-  const { toast } = useToast()
+  const isSmallScreen = useMediaQuery({ maxWidth: 640 }) // sm breakpoint
 
   const toggleRow = (id: number) => {
     setExpandedRows((prev) =>
@@ -56,20 +54,22 @@ export default function SentenceTable({ people }: SentenceTableProps) {
       <TableCaption>A list of your human collection.</TableCaption>
       <TableHeader>
         <TableRow>
-          <TableHead>No.</TableHead>
-          <TableHead>Name</TableHead>
-          <TableHead className="text-center">Sentence Count</TableHead>
-          <TableHead className="text-right">Actions</TableHead>
+          <TableHead className="hidden sm:table-cell">No.</TableHead>
+          <TableHead className="sm:text-left text-center">Name</TableHead>
+          <TableHead className="hidden text-center sm:table-cell">Sentence Count</TableHead>
+          <TableHead className="sm:text-right text-center">Actions</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
         {people.map((person, index) => (
           <>
             <TableRow key={person.id}>
-              <TableCell>{index + 1}</TableCell>
-              <TableCell>{person.name}</TableCell>
-              <TableCell className="text-center">{person.sentences.length}</TableCell>
-              <TableCell className="flex justify-end gap-2">
+              <TableCell className="hidden sm:table-cell">{index + 1}</TableCell>
+              <TableCell className="text-center sm:text-left">{person.name}</TableCell>
+              <TableCell className="hidden text-center sm:table-cell">
+                {person.sentences.length}
+              </TableCell>
+              <TableCell className="flex w-full flex-col justify-end gap-2 sm:flex-row">
                 <Button
                   variant="outline"
                   className="p-1 px-2"
@@ -93,24 +93,24 @@ export default function SentenceTable({ people }: SentenceTableProps) {
                     {person.sentences.map((sentence) => (
                       <div
                         key={sentence.id}
-                        className="grid grid-cols-[1fr,auto] gap-4 border-b border-muted-foreground/20 pb-4 last:border-b-0 last:pb-0"
+                        className="flex flex-col sm:grid grid-cols-[1fr,auto] gap-4 border-b border-muted-foreground/20 pb-4 last:border-b-0 last:pb-0"
                       >
                         <div className="grid grid-cols-2 gap-4">
                           <div>
-                            <p className="font-semibold">Sentence</p>
-                            <p>{sentence.sentence}</p>
+                            <p className="text-xs">Sentence</p>
+                            <p className="font-semibold text-md">{sentence.sentence}</p>
                           </div>
                           <div>
-                            <p className="font-semibold">Sentence count</p>
-                            <p>{sentence.sentenceCount}</p>
+                            <p className="text-xs">Sentence count</p>
+                            <p className="font-semibold text-md">{sentence.sentenceCount}</p>
                           </div>
                           <div>
-                            <p className="font-semibold">Elapsed time</p>
-                            <p>{sentence.elapsedTime}</p>
+                            <p className="text-xs">Elapsed time</p>
+                            <p className="font-semibold text-md">{sentence.elapsedTime}</p>
                           </div>
                           <div>
-                            <p className="font-semibold">Sentences per minute</p>
-                            <p>{sentence.sentencesPerMinute?.toFixed(2)}</p>
+                            <p className="text-xs">Sentences per minute</p>
+                            <p className="font-semibold text-md">{sentence.sentencesPerMinute?.toFixed(2)}</p>
                           </div>
                         </div>
                         <div className="flex flex-col justify-center gap-2">
@@ -132,12 +132,12 @@ export default function SentenceTable({ people }: SentenceTableProps) {
       </TableBody>
       <TableFooter className="bg-transparent hover:bg-transparent">
         <TableRow className="hover:bg-transparent">
-          <TableCell colSpan={2}>
+          <TableCell colSpan={isSmallScreen ? 1 : 2}>
             <Button variant="outline" disabled>
               Previous
             </Button>
           </TableCell>
-          <TableCell colSpan={2} className="text-right">
+          <TableCell colSpan={isSmallScreen ? 1 : 2} className="text-right">
             <Button variant="outline" disabled>
               Next
             </Button>
