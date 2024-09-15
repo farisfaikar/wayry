f62718fd99b49fc270c7fc92ea59c5c519a65188
Faris Faikar <farisfaikar.r@gmail.com>
2024-09-15 14:22:17 +0700

    remove: session-provider from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import './globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
 import Navbar from '@/components/nav/navbar'
-import { SessionProvider } from 'next-auth/react'
 
 export const metadata: Metadata = {
   title: 'Why Are You Repeating Yourself?',
@@ -20,9 +19,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           <Navbar />
           <main className="mx-auto max-w-2xl p-5 pt-16">
-            <SessionProvider>
-              {children}
-            </SessionProvider>
+            {children}
           </main>
         </ThemeProvider>
       </body>
