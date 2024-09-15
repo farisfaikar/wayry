74df004317f993e63c8dea9962ded33c27a4fa06
Faris Faikar <farisfaikar.r@gmail.com>
2024-09-15 14:27:59 +0700

    feat: call session in HomePage instead of SentenceForm to call it in a server component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import SentenceForm from '@/components/sentence-form'
+import { auth } from "@/server/auth"
 
-export default function HomePage() {
+export default async function HomePage() {
+  const session = await auth()
+  
   return (
     <>
       <h1 className="flex justify-center p-5 text-center text-4xl font-extrabold">
         Why Are You Repeating Yourself?
       </h1>
-      <SentenceForm />
+      <SentenceForm session={session} />
     </>
   )
 }
