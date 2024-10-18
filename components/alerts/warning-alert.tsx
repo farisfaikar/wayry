b837f5fa741bcff340c5a98ebbfb0f4e7afed786
Faris Faikar <farisfaikar.r@gmail.com>
2024-10-18 11:34:12 +0700

    hotfix: use mb-2 bg-primary for warning-alert
    format: minor formatting

--- components/alerts/warning-alert.tsx
+++ components/alerts/warning-alert.tsx
@@ -1,11 +1,11 @@
-import {  TriangleAlert } from "lucide-react"
+import { TriangleAlert } from "lucide-react"
 import React from "react"
 
-export default function WarningAlert({ message }: { message: string }){
+export default function WarningAlert({ message }: { message: string }) {
   return (
-    <div className="mt-5 flex w-full items-center gap-2  rounded-md  bg-[#FFF9DB] p-3 text-sm text-black">
+    <div className="mb-2 flex w-full items-center gap-2 rounded-md bg-primary p-3 text-sm text-black">
       <TriangleAlert className="h-6 w-6" />
-    <p>{message}</p>
-  </div>
+      <p>{message}</p>
+    </div>
   )
 }
